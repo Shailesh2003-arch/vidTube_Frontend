51d44a2c20980b04793a855cb687bc5b705171cc
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-08-27 13:44:33 +0530

    fix layout

--- src/FeedLayout.jsx
+++ src/FeedLayout.jsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 
 export const FeedLayout = () => {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex-1 min-w-0 pb-16 md:pb-0 overflow-x-hidden">
         <Outlet />
       </div>
     </div>
