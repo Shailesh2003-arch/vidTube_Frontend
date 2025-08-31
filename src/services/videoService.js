447baf27fa5960adc94704a9077cd409954a4076
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-08-31 11:48:35 +0530

    add delte-video API call

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 export const fetchHomePageVideos = async (page, limit = 10) => {
   const res = await fetch(
     `http://localhost:4000/api/v1/videos/homepage?page=${page}&limit=${limit}`,
@@ -7,5 +8,19 @@ export const fetchHomePageVideos = async (page, limit = 10) => {
   );
   const data = await res.json();
   if (!res.ok) throw new Error(data.message || "Failed to fetch videos");
+  return data;
+};
+
+export const deleteVideoFromthePlaylist = async (playlistId, videoId) => {
+  const res = await fetch(
+    `http://localhost:4000/api/v1/users/playlist/remove/${playlistId}/${videoId}`,
+    {
+      credentials: "include",
+      method: "PATCH",
+    }
+  );
+  const data = await res.json();
+  if (!res.ok) throw new Error(data.message || "Failed to fetch videos");
+  toast.success("Removed Video from the Playlist");
   return data;
 };
