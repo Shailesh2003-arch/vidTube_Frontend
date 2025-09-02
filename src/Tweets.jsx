84707a72e35505dc75ce4fb81910194dd5095c63
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-02 22:00:34 +0530

    add tweets page

--- src/Tweets.jsx
+++ src/Tweets.jsx
@@ -1,14 +1,23 @@
+import { TweetCard } from "./components/TweetCard";
+import { useTweets } from "./hooks/useTweets";
 export const Tweets = () => {
-  return (
-    <div className="flex h-screen ">
-      {/* Main content */}
-      <div className="flex-1 p-4 flex flex-col ">
-        <div className="flex items-end justify-end gap-4">
-          {/* <NotificationBell /> */}
-        </div>
+  const { tweets } = useTweets();
 
-        <h2 className="text-xl font-bold mb-4">Tweets</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 overflow-y-scroll h-[80vh] hide-scrollbar "></div>
+  return (
+    <div className="flex h-screen">
+      {/* Main content */}
+      <div className="flex-1 p-2 flex flex-col">
+        {/* Heading fixed at top */}
+        <h2 className="text-xl font-bold mb-4 sticky top-0 bg-white dark:bg-gray-900 z-10 p-2">
+          Tweets
+        </h2>
+
+        {/* Scrollable tweets */}
+        <div className="flex-1 overflow-y-auto  columns-1 sm:columns-2 lg:columns-3 gap-4">
+          {tweets.map((tweet) => (
+            <TweetCard key={tweet._id} tweet={tweet} />
+          ))}
+        </div>
       </div>
     </div>
   );
