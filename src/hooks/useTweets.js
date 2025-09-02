0848b8395c655168c7a5afbc34e3a491af5750d9
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-02 22:02:18 +0530

    add createTweet API call

--- src/hooks/useTweets.js
+++ src/hooks/useTweets.js
@@ -20,6 +20,7 @@ export const useTweets = () => {
         }
       );
       setTweets(res.data.data);
+      console.log(res);
     } catch (err) {
       console.error("Error fetching tweets", err);
     } finally {
@@ -28,8 +29,76 @@ export const useTweets = () => {
   };
 
   useEffect(() => {
-    fetchMyTweets();
-  }, []);
+    if (userId) fetchMyTweets();
+  }, [userId]);
 
-  return { loading, error, fetchMyTweets, tweets };
+  const createTweet = async (tweetData) => {
+    try {
+      let res;
+      if (tweetData instanceof FormData) {
+        res = await fetch("http://localhost:4000/api/v1/users/tweets/create", {
+          method: "POST",
+          body: tweetData,
+          credentials: "include",
+        });
+      } else {
+        res = await fetch("http://localhost:4000/api/v1/users/tweets/create", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(tweetData),
+          credentials: "include",
+        });
+      }
+      const data = await res.json();
+      if (res.ok) {
+        await fetchMyTweets();
+      } else {
+        alert(data.message || "Failed to post tweet");
+      }
+    } catch (error) {
+      console.log(`Error creating the Tweet`, error.message);
+    }
+  };
+
+  const updateTweet = async (tweetId, tweetData) => {
+    try {
+      let res;
+      if (tweetData instanceof FormData) {
+        res = await fetch(
+          `http://localhost:4000/api/v1/users/tweets/${tweetId}`,
+          {
+            method: "PATCH",
+            body: tweetData,
+            credentials: "include",
+          }
+        );
+      } else {
+        res = await fetch(
+          `http://localhost:4000/api/v1/users/tweets/${tweetId}`,
+          {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(tweetData),
+            credentials: "include",
+          }
+        );
+      }
+
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message || "Failed to update tweet");
+      return data;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
+
+  return {
+    loading,
+    error,
+    fetchMyTweets,
+    tweets,
+    createTweet,
+    updateTweet,
+  };
 };
