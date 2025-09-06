a9b24def49c65cb3ffbd5f8dd19f0a045f8907cd
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-09-06 22:02:14 +0530

    modify fetch -> axios

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,14 +1,25 @@
 import { toast } from "react-toastify";
-export const fetchHomePageVideos = async (page, limit = 10) => {
-  const res = await fetch(
-    `http://localhost:4000/api/v1/videos/homepage?page=${page}&limit=${limit}`,
-    {
-      credentials: "include",
+import axios from "axios";
+
+export const fetchHomePageVideos = async (cursor = null, limit = 10) => {
+  try {
+    const res = await axios.get(
+      "http://localhost:4000/api/v1/videos/homepage",
+      {
+        withCredentials: true,
+        params: { limit, cursor },
+      }
+    );
+    console.log(res);
+    return res.data; // { videos, nextCursor }
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || "Failed to fetch videos"
+      );
     }
-  );
-  const data = await res.json();
-  if (!res.ok) throw new Error(data.message || "Failed to fetch videos");
-  return data;
+    throw new Error("Unexpected error occurred");
+  }
 };
 
 export const deleteVideoFromthePlaylist = async (playlistId, videoId) => {
