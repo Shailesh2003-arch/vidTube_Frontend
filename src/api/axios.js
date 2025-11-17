20a59ea37937327866916758221be278bc6e2bce
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-17 17:28:28 +0530

    add axios instance for api calls

---
+++ src/api/axios.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000/api/v1",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // If token expired AND we haven't retried this request yet
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await api.post("/users/refresh-token"); // hits your backend refresh route
+        return api(originalRequest); // retry actual API call
+      } catch (err) {
+        console.log("Refresh failed. Logging out...");
+        localStorage.removeItem("userInfo");
+        window.location.href = "/register";
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
