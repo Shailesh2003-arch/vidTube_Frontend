be68f721ae0acf3bf0bb13e411e806bf5a8519c4
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-17 17:58:29 +0530

    add user-profile-header

--- src/components/UserProfileHeader.jsx
+++ src/components/UserProfileHeader.jsx
@@ -1,5 +1,7 @@
 // src/components/UserProfileHeader.jsx
 // src/components/UserProfileHeader.jsx
+
+import { Link } from "react-router-dom";
 export const UserProfileHeader = ({ avatar, fullName, username }) => {
   return (
     <div className="flex flex-col items-center gap-4 p-4 md:flex-row md:items-start md:gap-6 w-full overflow-hidden ">
@@ -15,19 +17,17 @@ export const UserProfileHeader = ({ avatar, fullName, username }) => {
         <h2 className="text-2xl font-bold">{fullName}</h2>
         <p className="text-gray-400">
           @{username} •{" "}
-          <span className="text-blue-400 cursor-pointer">View channel</span>
+          <Link
+            to={`/feed/channel/${username}`}
+            className="text-blue-400 cursor-pointer hover:underline hover:text-blue-500 transition-colors"
+          >
+            View channel
+          </Link>
         </p>
       </div>
 
       {/* Buttons */}
-      <div className="flex gap-3 mt-4 md:mt-0 md:ml-auto flex-wrap justify-center md:justify-end">
-        <button className="px-4 py-2 rounded-lg border border-black/20 dark:border-white/20 bg-white dark:bg-gray-800 flex items-center gap-2 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-          <span>Switch account</span>
-        </button>
-        <button className="px-4 py-2 rounded-lg border border-black/20 dark:border-white/20 bg-white dark:bg-gray-800 flex items-center gap-2 hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-          <span>Google Account</span>
-        </button>
-      </div>
+      <div className="flex gap-3 mt-4 md:mt-0 md:ml-auto flex-wrap justify-center md:justify-end"></div>
     </div>
   );
 };
