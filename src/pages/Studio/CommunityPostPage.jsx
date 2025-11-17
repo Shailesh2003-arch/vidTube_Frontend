2354c5d57047c0eb01bb0a0bb958b465e5ca5eee
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-11-17 20:29:44 +0530

    delete communitypostpage

--- src/pages/Studio/CommunityPostPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function CommunityPostPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Channel Analytics</h1>
-      <p className="mt-2 text-gray-400">Insights will be displayed here.</p>
-    </div>
-  );
-}
